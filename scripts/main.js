53b3c8b3eab320f03563640d99c54809074aeeca
frvnk <frvnkliu@gmail.com>
2025-04-16 15:11:34 -0400

    added start button

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,106 @@
-//const cage1 = document.getElementById("cage1");
+import * as THREE from './three.module.js';
 
 let offsetX, offsetY, draggedCage = null;
 
-let cages = document.getElementsByClassName("cage");
+let cages = Array.from(document.getElementsByClassName("cage"));
+let cageBackgrounds = Array.from(document.getElementsByClassName("cageBackground"));
 
+const cageMap = new Map();
 
+cages.forEach((cage, index) => {
+  // Use the same index to access the corresponding cageBackground.
+  const background = cageBackgrounds[index];
+  cageMap.set(cage, background);
+});
+
+function updateCages(currCage){
+    let i = cages.indexOf(currCage);
+    if (i === -1) return; // Cage not found
+
+    // Remove the current cage from its position
+    cages.splice(i, 1);
+
+    // Push it to the end (most recently used)
+    cages.push(currCage);
+
+    // Update z-indexes based on order in array
+    for (let z = 0; z < cages.length; z++) {
+        cages[z].style.zIndex = z+3;
+        cageMap.get(cages[z]).style.zIndex = z;
+    }
+}
+
+function isOverlap(cageA, cageB){
+    const cageLength = cageA.clientHeight;
+    const cageAX = cageA.getBoundingClientRect().left;
+    const cageAY = cageA.getBoundingClientRect().top;
+    const cageBX = cageB.getBoundingClientRect().left;
+    const cageBY = cageB.getBoundingClientRect().top;
+    const diffX = Math.abs(cageAX-cageBX);
+    const diffY = Math.abs(cageAY-cageBY);
+
+    const pullThresh = 0.5;
+    console.log(`Cage Length: ${cageLength}`)
+    console.log(`X: ${diffX}, Y: ${diffY}`);
+    return diffX < pullThresh*cageLength && diffY < pullThresh*cageLength;
+}
+
+// start dragging
 for (let cage of cages) {
-    cage.addEventListener("mousedown", (e) => {
+    // ensure touch-action:none is in effect
+    cage.addEventListener("pointerdown", e => {
+        e.preventDefault();            // stop text‐selection, scrolling, etc.
         draggedCage = cage;
-        offsetX = e.clientX - cage.getBoundingClientRect().left;
-        offsetY = e.clientY - cage.getBoundingClientRect().top;
+        updateCages(draggedCage);
+
+        const rect = cage.getBoundingClientRect();
+        offsetX = e.clientX - rect.left;
+        offsetY = e.clientY - rect.top;
+        cage.setPointerCapture(e.pointerId);
         cage.style.cursor = "grabbing";
     });
 }
 
-document.addEventListener("mousemove", (e) => {
-    if (draggedCage) {
-        /* Add min and max so it doesn't go out of bounds*/
-        draggedCage.style.left = `${e.clientX - offsetX}px`;
-        draggedCage.style.top = `${e.clientY - offsetY}px`;
-    }
+// while dragging
+document.addEventListener("pointermove", e => {
+    if (!draggedCage) return;
+
+    const vw = window.innerWidth;
+    const vh = window.innerHeight;
+
+    let leftPx = e.clientX - offsetX;
+    let topPx  = e.clientY - offsetY;
+
+    const leftVw = (leftPx / vw) * 100;
+    const topVh  = (topPx / vh) * 100;
+
+    draggedCage.style.left = `${leftVw}vw`;
+    draggedCage.style.top  = `${topVh}vh`;
+
+    const bg = cageMap.get(draggedCage);
+    bg.style.left = `${leftVw}vw`;
+    bg.style.top  = `${topVh}vh`;
 });
 
-document.addEventListener("mouseup", () => {
+// end dragging
+document.addEventListener("pointerup", e => {
+    if (!draggedCage) return;
+
+    draggedCage.releasePointerCapture(e.pointerId);
     draggedCage.style.cursor = "grab";
+
+    for (let cage of cages) {
+        if (cage !== draggedCage && isOverlap(draggedCage, cage)) {
+        console.log("Cage Overlap:", cage.id, draggedCage.id);
+        // do overlap logic…
+        }
+    }
     draggedCage = null;
+});
+
+
+const startText = document.getElementById("start");
+
+startText.addEventListener("click", ()=>{
+
 });
--- scripts/spirit.js
+++ scripts/spirit.js
@@ -1,30 +1,31 @@
 import * as THREE from './three.module.js';
 
-const cage1 = document.getElementById("cage1");
-const cage2 = document.getElementById("cage2");
 const container = document.getElementById("container");
 
 const scene = new THREE.Scene();
 
 const axesHelper = new THREE.AxesHelper(100);
 
-// Add it to the scene
+const cage1 = document.getElementById("cage1");
+const cage2 = document.getElementById("cage2");
+const cage3 = document.getElementById("cage3");
+
+/* === Cage 1 Beginning === */
 scene.add(axesHelper);
 // Create two separate cameras
-let camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
-camera.position.set(0, 0, 180);
+let camera = new THREE.PerspectiveCamera(75, 1, 1, 1000);
+camera.position.set(0, 0, 125);
 
 
 // Create two renderers, one for each div
 let renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setSize(container.clientWidth, container.clientHeight);
-container.appendChild(renderer.domElement);
-
+renderer.setSize(cage1.clientWidth, cage1.clientHeight);
+cage1.appendChild(renderer.domElement);
 
 // Group both the solid object and the wireframe
-let spirit1 = new THREE.Group();
+let spirit = new THREE.Group();
 
-let torusGeometry = new THREE.TorusKnotGeometry(21, 6, 64, 8, 6, 9);
+let torusGeometry = new THREE.TorusKnotGeometry(42, 12, 64, 8, 6, 9);
 
 const solidMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
 
@@ -34,47 +35,22 @@ let wireframe = new THREE.LineSegments(edgesGeometry, edgesMaterial);
 
 let solidTorusKnot = new THREE.Mesh(torusGeometry, solidMaterial);
 
-spirit1.add(solidTorusKnot);
-spirit1.add(wireframe);
+spirit.add(solidTorusKnot);
+spirit.add(wireframe);
 
 const centerSphereGeometry = new THREE.SphereGeometry(5, 16, 16); // Small sphere
 const centerSphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 }); // Red color
 const centerSphere = new THREE.Mesh(centerSphereGeometry, centerSphereMaterial);
 // Add the sphere to the spirit group
-spirit1.add(centerSphere);
-spirit1.position.set(0,0,0);
-scene.add(spirit1);
-
-const clock = new THREE.Clock();
+scene.add(centerSphere);
+spirit.position.set(0,0,0);
+scene.add(spirit);
 
 let tubularSegments = 64;
 let increasing = true; // Direction flag
 const minSegments = 3;
 const maxSegments = 64;
 const speed = 0.5; // Adjust this for smoother/slower animation
-
-let spirit2 = new THREE.Group();
-const centerSphere2 = new THREE.Mesh(centerSphereGeometry, centerSphereMaterial);
-spirit2.add(centerSphere2);
-spirit2.position.set(300,-300,0);
-
-// Create a tetrahedron with radius 42
-const tetraGeometry = new THREE.TetrahedronGeometry(21); // Radius of 42
-const tetraMaterial = new THREE.MeshBasicMaterial({ color: 0xffaa00, wireframe: true }); // Orange wireframe
-const tetrahedron = new THREE.Mesh(tetraGeometry, tetraMaterial);
-
-// Add tetrahedron to spirit2
-spirit2.add(tetrahedron);
-const axesHelper2 = new THREE.AxesHelper(100);
-
-// Add it to the scene
-spirit2.add(axesHelper2);
-
-
-scene.add(spirit2);
-
-
-
 
 let rotationSpeed1 = 0.05;
 function updateSpirit1(deltaTime){
@@ -106,46 +82,130 @@ function updateSpirit1(deltaTime){
     wireframe.geometry.dispose();
     wireframe.geometry = edgesGeometry;
 
-    spirit1.rotation.x += rotationSpeed1 * deltaTime;
-    spirit1.rotation.y += rotationSpeed1 * deltaTime;
+    spirit.rotation.x += rotationSpeed1 * deltaTime;
+    spirit.rotation.y += rotationSpeed1 * deltaTime;
+}
+/* === Cage 1 End === */
+
+
+/* === Cage 2 Beginning === */
+
+const scene2 = new THREE.Scene();
+// Create two separate cameras
+const camera2 = new THREE.PerspectiveCamera(75, 1, 1, 1000);
+camera2.position.set(0, 0, 125);
+
+
+// Create two renderers, one for each div
+let renderer2 = new THREE.WebGLRenderer({ alpha: true });
+renderer2.setSize(cage2.clientWidth, cage2.clientHeight);
+cage2.appendChild(renderer2.domElement);
+
+let ringsGroup = new THREE.Group();
+const centerSphere2 = new THREE.Mesh(centerSphereGeometry, centerSphereMaterial);
+
+ringsGroup.add(centerSphere2);
+
+function createRing(ringParams){
+    const ring = new THREE.group();
+
+    // Define outer and inner radii
+    const outerRadius = 72;
+    const innerRadius = 70;
+
+    // Create a hollow cylinder using a CylinderGeometry with different radii
+    const ringShape = new THREE.Shape();
+    ringShape.absarc(0, 0, outerRadius, 0, Math.PI * 2, false);
+
+    const holePath = new THREE.Path();
+    holePath.absarc(0, 0, innerRadius, 0, Math.PI * 2, true);
+    ringShape.holes.push(holePath);
+
+    const extrudeSettings = {
+        steps: 2,
+        depth: 16,
+        bevelEnabled: false,
+        bevelThickness: 2,
+        bevelSize: 0,
+        bevelOffset: 0,
+        bevelSegments: 1
+    };
+
+    const ringGeometry = new THREE.ExtrudeGeometry(ringShape, extrudeSettings);
+    const ringMaterial = new THREE.MeshStandardMaterial({
+        color: 0x000000, // Red
+        wireframe: true, // Enables wireframe mode
+        transparent: true,
+        opacity: 0.5 // Optional transparency
+    });
+    const ringMesh = new THREE.Mesh(ringGeometry, ringMaterial);
+    scene.add(ringMesh);
+
+    ringMesh.position.set(0, 0, -8);
+    return ring;
 }
 
+scene2.add(ringsGroup);
+const rings = [];
+const rotations = [
+    [0, 1, 0],
+    [1, 1 ,0]
+];
 
+const ringRotationSpeeds = [
+    [0.10, 0.005],
+    [0.15, 0.010]
+]
 
-
-let aspectRatio = window.innerWidth / (window.innerHeight);
-
-function updatePosition(X, Y){
-
+function updateRings(deltaTime){
+    for(let i = 0; i < rings.length; i++){
+        const ring = rings[i];
+        ring.rotation.z += ringRotationSpeeds[i][0] * deltaTime;
+        ring.rotateOnWorldAxis(new THREE.Vector3(rotations[i]), ringRotationSpeeds[i][1]*deltaTime);
+    }
 }
 
+/* === Cage 2 End === */
+
+/* === Cage 3 Beginning === */
+const scene3 = new THREE.Scene();
+// Create two separate cameras
+const camera3 = new THREE.PerspectiveCamera(75, 1, 1, 1000);
+camera3.position.set(0, 0, 125);
+
+// Create two renderers, one for each div
+let renderer3 = new THREE.WebGLRenderer({ alpha: true });
+renderer3.setSize(cage3.clientWidth, cage3.clientHeight);
+cage3.appendChild(renderer3.domElement);
+const centerSphere3 = new THREE.Mesh(centerSphereGeometry, centerSphereMaterial);
+
+scene3.add(centerSphere3);
+
+
+/* === Cage 3 End === */
+
+const clock = new THREE.Clock();
 function animate() {
     requestAnimationFrame(animate);
     // Get the time delta in seconds since the last frame
     const deltaTime = clock.getDelta()*10;
 
     updateSpirit1(deltaTime);
+    updateRings(deltaTime);
     // Render the scene
 
-    /*
-    const direction = new THREE.Vector3();
-    camera1.getWorldDirection(direction);
-    console.log("Camera 1: ", direction);
-    camera2.getWorldDirection(direction);
-    console.log("Camera 2: ", direction);
-    */
-
     renderer.render(scene, camera);
+    renderer2.render(scene2, camera2);
+    renderer3.render(scene3, camera3);
 }
 
 animate();
 
-
-
-
 function onWindowResize() {
     // Update the size of the renderer
-    renderer.setSize(container.clientWidth, container.clientHeight);
+    renderer.setSize(cage1.clientWidth, cage1.clientHeight);
+    renderer2.setSize(cage2.clientWidth, cage2.clientHeight);
+    renderer3.setSize(cage3.clientWidth, cage3.clientHeight);
 }
 
 // Add event listener for window resize
